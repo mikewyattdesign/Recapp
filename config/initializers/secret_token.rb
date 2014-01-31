fd96147cb41dcc0c6433fb3d2da81100837ed334
Mike Wyatt II <mike@mikewyattdesign.com>
2014-01-31 16:17:31 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+G360EventApplication::Application.config.secret_key_base = '216b85390259abaa1afde9daaca13635caa61da6746b48c2b39db7411a93795795987edd17a9deeba14cb20bf5468fae6fbd3919d6875a78fe61cbb75d2f1512'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+G360EventApplication::Application.config.session_store :cookie_store, key: '_g360-event-application_session'
