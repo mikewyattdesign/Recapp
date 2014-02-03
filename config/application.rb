2314e9928d8019ae4bd836d82922c4454c8b7ab5
Mike Wyatt II <mike@mikewyattdesign.com>
2014-02-03 10:38:19 -0600

    install bower and update asset pipeline path to include vendor/assets/bower_components folder

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,10 @@ module G360EventApplication
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+    config.assets.precompile += %w( .svg .eot .woff .ttf )
   end
 end
