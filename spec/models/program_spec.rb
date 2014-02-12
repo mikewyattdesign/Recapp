474137d53e80f90b0fe45312ba037da7b38a715f
Michael Wyatt <michaelwyatt@group360.com>
2014-02-12 10:45:24 -0600

    Setup program spec skeleton

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Program do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it "is valid with a name, and brand_id"
+	it "is invalid without a name"
+	it "is invalid without a brand_id"
+	it "responds to brands"
+	it "returns its brand"
+	it "responds to events"
 end
