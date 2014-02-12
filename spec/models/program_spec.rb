c31448821d153929bd528db20c3962e9bb7ddc03
Michael Wyatt <michaelwyatt@group360.com>
2014-02-12 10:58:28 -0600

    NUT-74 #resolve #time 20m #comment add program model spec

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -1,10 +1,25 @@
 require 'spec_helper'
 
 describe Program do
-	it "is valid with a name, and brand_id"
-	it "is invalid without a name"
-	it "is invalid without a brand_id"
-	it "responds to brands"
-	it "returns its brand"
-	it "responds to events"
+	it "is valid with a name, and brand_id" do
+		program = Program.new(name: 'Heroes', brand_id: 1)
+		expect(program).to be_valid
+	end
+	it "is invalid without a name" do
+		expect(Program.new(name: nil)).to have(1).errors_on(:name)
+	end
+	it "is invalid without a brand_id" do
+		expect(Program.new(brand_id: nil)).to have(1).errors_on(:brand_id)
+	end
+	it "responds to brand" do
+		expect(Program.new(name: 'Heroes', brand_id: 1)).to respond_to(:brand)
+	end
+	it "returns its brand" do
+		brand = Brand.create(name: 'Busch')
+		program = Program.new(name: 'Heroes', brand_id: brand.id)
+		expect(program.brand).to eq brand
+	end
+	it "responds to events" do
+		expect(Program.new(name: 'Heroes', brand_id: 1)).to respond_to(:events)
+	end
 end
