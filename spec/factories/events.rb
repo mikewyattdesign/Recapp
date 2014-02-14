4519d72c1718d8a98b10f94167abedcc0b0ed073
Michael Wyatt <michaelwyatt@group360.com>
2014-02-14 14:44:49 -0600

    Fix back button within edit event giveaways and add location to event factory

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -6,5 +6,10 @@ FactoryGirl.define do
 		start_date_time Date.yesterday
 		end_date_time Date.today
 		program_id 1
+		street { Faker::Address.street_address }
+		city { Faker::Address.city }
+		state { Faker::Address.state }
+		postal_code { Faker::Address.zip }
+		country { Faker::Address.country }
 	end
 end
