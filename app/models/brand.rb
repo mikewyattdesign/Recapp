401671ac88a5749df506e04188e5db9913fb2cf1
Michael Wyatt <michaelwyatt@group360.com>
2014-03-11 10:52:15 -0500

    Add brand image styles and add giveaways to main navigation

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,7 +2,7 @@ class Brand < ActiveRecord::Base
     has_many :programs
     has_many :assignments, as: :assignable
     has_many :users, through: :assignments
-    has_attached_file :logo
+    has_attached_file :logo, styles: {large: '700x700', medium: '500x500', small: '300x300'}
     validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\z/
 
     validates :name, presence: true, uniqueness: true
