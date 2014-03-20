3d32f41f2c749a562f0027faf8bd9091300f92a2
Michael Wyatt <michaelwyatt@group360.com>
2014-03-20 10:53:19 -0500

    Add brand update test

--- spec/features/brand_management_spec.rb
+++ spec/features/brand_management_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 feature 'Brand Management' do
+	before {@brand = build(:brand)}
 	scenario 'User creates a brand' do
-		@brand = build(:brand)
 		visit brands_path
 		click_link 'New Brand'
 		fill_in 'Name', with: @brand.name
 		click_button 'Create Brand'
+		expect(current_path).to eq("/brands")
+		expect(page).to have_content "#{@brand.name}"
+	end
+	scenario 'User updates a brand' do
+		@original_brand = create(:brand)
+		visit edit_brand_path(@original_brand)
+		fill_in 'Name', with: @brand.name
+		click_button 'Update Brand'
 		expect(current_path).to eq("/brands")
 		expect(page).to have_content "#{@brand.name}"
 	end
