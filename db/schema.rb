ac71057b4f90bf46dc43f3def5543529699e521b
Michael Wyatt <michaelwyatt@group360.com>
2014-03-27 12:30:25 -0500

    temporarily default users to be admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140327150550) do
+ActiveRecord::Schema.define(version: 20140327172644) do
 
   create_table "assignments", force: true do |t|
     t.integer  "user_id"
@@ -159,7 +159,7 @@ ActiveRecord::Schema.define(version: 20140327150550) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "role",                   default: 4
+    t.integer  "role",                   default: 0
     t.string   "username"
   end
 
