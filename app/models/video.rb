201aa53987fbda152656c797d064e33e4e6310c4
Michael Wyatt <michaelwyatt@group360.com>
2014-04-11 11:37:55 -0500

    add Paperclip-ffmpeg gem for video thumbnail processing

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,7 @@
 class Video < ActiveRecord::Base
 	belongs_to :event
-    has_attached_file :media    
+    has_attached_file :media, styles: {thumb: {geometry: "300x300", format: 'jpg', time: 10}}, processors: [:ffmpeg]    
     validates_attachment_content_type :media, :content_type => /\Avideo\/.*\z/
+    validates :media, presence: true
+    validates :event_id, presence: true
 end
