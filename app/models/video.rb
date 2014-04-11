ad7d355acde48273292652ec2b965b2866faffbe
Michael Wyatt <michaelwyatt@group360.com>
2014-04-11 15:15:24 -0500

    swap presence requirement methods on media

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,6 +2,6 @@ class Video < ActiveRecord::Base
 	belongs_to :event
     has_attached_file :media, styles: {thumb: {geometry: "300x300", format: 'jpg', time: 10}}, processors: [:ffmpeg]    
     validates_attachment_content_type :media, :content_type => /\Avideo\/.*\z/
-    validates :media, presence: true
+    validates_attachment_presence :media
     validates :event_id, presence: true
 end
