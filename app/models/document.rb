300243e6b43e1e0df028a377f75cb9ebb199df0b
Michael Wyatt <michaelwyatt@group360.com>
2014-04-14 16:24:54 -0500

    Add event, program, and brand methods to document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -3,4 +3,30 @@ class Document < ActiveRecord::Base
   has_attached_file :doc
   validates_attachment_content_type :doc, content_type: /\Aapplication\/msword|vnd.openxmlformats-officedocument.wordprocessingml.document|vnd\.ms-excel|vnd\.ms-powerpoint|pdf/
   validates_attachment_presence :doc
+
+    def self.with_event
+    	Document.where(id: (Document.where(documentable_type: 'Event').reject{|p| p.event.nil?}.map{|p|p.id}))
+    end
+
+    def event
+    	if documentable_type == Event.name 
+    		Event.find(documentable_id) if Event.where(id: documentable_id).count > 0
+    	end
+    end
+
+    def event_id
+        event.id if event.present?
+    end
+
+    def program_id
+        event.program.id if event.present?
+    end
+
+    def brand_id
+        event.program.brand_id if event.present?
+    end
+
+    def image_url
+        image.url if image.present?
+    end
 end
