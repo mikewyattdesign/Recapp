d19c2ed4d907147140bde7c36229ac227a37246a
Michael Wyatt <michaelwyatt@group360.com>
2014-04-18 12:20:13 -0500

    change default role to guest

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417213112) do
+ActiveRecord::Schema.define(version: 20140418161628) do
 
   create_table "assignments", force: true do |t|
     t.integer  "user_id"
@@ -181,7 +181,7 @@ ActiveRecord::Schema.define(version: 20140417213112) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "role",                   default: 0
+    t.integer  "role",                   default: -1
     t.string   "username"
   end
 
