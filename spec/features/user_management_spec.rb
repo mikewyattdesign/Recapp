07565de533e16cc40a5c44e86d909c471fdb3961
Michael Wyatt <michaelwyatt@group360.com>
2014-04-24 13:47:13 -0500

    fix user management spec error

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'User Management' do
 	before {
 		3.times {create(:brand)}
-		Brand.each {|brand| 3.times{ create(:program, brand_id: brand.id)} }
+		Brand.all.each {|brand| 3.times{ create(:program, brand_id: brand.id)} }
 		@brand = Brand.first
 		@program = @brand.programs.first
 		@user = create(:user)
