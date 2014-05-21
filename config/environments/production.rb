3cb03e5303fd3dbca6ff5ae4d08ff366a6944599
Adam Bowen <adambowen@group360.com>
2014-05-21 14:54:17 -0500

    Use mandrill for SMTP on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,14 @@ G360EventApplication::Application.configure do
             :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
         }
     }
+
+    ActionMailer::Base.smtp_settings = {
+        port:           '587',
+        address:        'smtp.mandrillapp.com',
+        user_name:      ENV['MANDRILL_USERNAME'],
+        password:       ENV['MANDRILL_APIKEY'],
+        domain:         'heroku.com',
+        authentication: :plain
+    }
+    ActionMailer::Base.delivery_method = :smtp
 end
