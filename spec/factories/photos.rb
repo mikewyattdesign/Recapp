a9a72815fcd7347edd7db2fa8bfb97a025478761
Michael Liu <michaelliu@group360.com>
2014-11-11 13:16:51 -0600

    Added test to check that filtering by date works

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -2,6 +2,8 @@ FactoryGirl.define do
     factory :photo do
         image File.new("#{Dir.pwd}/spec/support/IMG_5024.jpg")
 
+        created_at Date.today
+
         after(:build) do |user|
             user.class.skip_callback(:create, :after, :queue_processing)
         end
