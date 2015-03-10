dbc79e63ad20692a48782b8475a33e14c98f84b6
Michael Liu <michaelliu@group360.com>
2015-03-10 15:35:35 -0500

    Change binaries for wkhtmltopdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,13 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  exe_path: ENV['RAILS_ENV'] == 'production' ? "#{Rails.root}/bin/wkhtmltopdf-amd64" : '/opt/boxen/rbenv/versions/2.1.5/bin/wkhtmltopdf'
-}
+# WickedPdf.config = {
+#   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
+#   #:layout => "pdf.html",
+#   exe_path: ENV['RAILS_ENV'] == 'production' ? "#{Rails.root}/bin/wkhtmltopdf-amd64" : '/opt/boxen/rbenv/versions/2.1.5/bin/wkhtmltopdf'
+# }
+
+if Rails.env.production?
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-amd64"
+else
+  wkhtmltopdf_path = "#{Rails.root}/bin/wkhtmltopdf-0.9.9-OS-X.i368"
+end
+
+WickedPdf.config = { exe_path: wkhtmltopdf_path, wkhtmltopdf: wkhtmltopdf_path }
