019c6884d2f0ff5247e84c2dabf668ece33faa0e
Michael Liu <michaelliu@group360.com>
2015-05-04 17:30:28 -0500

    Modify contact validations to allow blank fields

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,8 @@ class Contact < ActiveRecord::Base
 
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email, format: { with: /@/, message: "Please use valid email" }
-    validates :mobile_phone, format: { with: /\d{3} \d{3} \d{4}/, message: "Please use the 'xxx xxx xxxx' format" }
-    validates :desk_phone, format: { with: /\d{3} \d{3} \d{4}/, message: "Please use the 'xxx xxx xxxx' format" }
+    validates :state, length: { is: 2 }, allow_blank: true
+    validates :email, format: { with: /@/, message: "Please use valid email" }, allow_blank: true
+    validates :mobile_phone, format: { with: /\d{3} \d{3} \d{4}/, message: "Please use the 'xxx xxx xxxx' format" }, allow_blank: true
+    validates :desk_phone, format: { with: /\d{3} \d{3} \d{4}/, message: "Please use the 'xxx xxx xxxx' format" }, allow_blank: true
 end
