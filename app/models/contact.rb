456dc9237a5ed483bade1f7f20dc6ede2bbb13a1
Michael Liu <michaelliu@group360.com>
2015-05-05 12:12:08 -0500

    Add contacts dynamically to event show page and report page

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,8 +5,8 @@ class Contact < ActiveRecord::Base
     validates :last_name, presence: true
     validates :state, length: { is: 2 }, allow_blank: true
     validates :email, format: { with: /@/, message: "Please use valid email" }, allow_blank: true
-    validates :mobile_phone, format: { with: /\d{3} \d{3} \d{4}/, message: "Please use the 'xxx xxx xxxx' format" }, allow_blank: true
-    validates :desk_phone, format: { with: /\d{3} \d{3} \d{4}/, message: "Please use the 'xxx xxx xxxx' format" }, allow_blank: true
+    validates :mobile_phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Please use the 'xxx-xxx-xxxx' format" }, allow_blank: true
+    validates :desk_phone, format: { with: /\d{3}-\d{3}-\d{4}/, message: "Please use the 'xxx-xxx-xxxx' format" }, allow_blank: true
 
     def full_name
         "#{first_name} #{last_name}"
@@ -14,5 +14,13 @@ class Contact < ActiveRecord::Base
 
     def city_state_zip
         "#{city}, #{state} #{zip}"
+    end
+
+    def m_phone
+        "<span class='brand-primary'>M:</span> #{mobile_phone}".html_safe unless mobile_phone.empty?
+    end
+
+    def d_phone
+        "<span class='brand-primary'>D:</span> #{desk_phone}".html_safe unless desk_phone.empty?
     end
 end
