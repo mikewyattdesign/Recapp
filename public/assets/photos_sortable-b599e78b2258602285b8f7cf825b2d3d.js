a67c53c79ab56ee774fc4501363a94f6299584d4
Michael Liu <michaelliu@group360.com>
2015-05-07 16:18:40 -0500

    Add precompiled assets for photo ordering

---
+++ public/assets/photos_sortable-b599e78b2258602285b8f7cf825b2d3d.js
@@ -0,0 +1,28 @@
+(function() {
+  $(function() {
+    var sortable_index;
+    sortable_index = function(sortable_list, event_type) {
+      sortable_list.each(function(index) {
+        $.post('set_favorite_photo_order', {
+          event_id: $(this).data('event-id'),
+          photo_id: $(this).data('photo-id'),
+          order: index,
+          event_type: event_type
+        });
+      });
+    };
+    $('ul.sortable').sortable({
+      connectWith: 'ul',
+      placeholder: 'drop-placeholder',
+      forcePlaceholderSize: true
+    });
+    $('#sortable_favorites').on('sortupdate sortreceive', function(event, ui) {
+      sortable_index($('#sortable_favorites').sortable('widget').children(), event.type);
+    });
+    $('#sortable_favorites').on('sortremove', function(event, ui) {
+      sortable_index(ui.item, event.type);
+    });
+    $('#sortable_favorites, #sortable_other').disableSelection();
+  });
+
+}).call(this);
