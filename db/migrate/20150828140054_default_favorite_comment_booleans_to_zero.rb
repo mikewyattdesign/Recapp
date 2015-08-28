549de500aad49df850e20e081df3cc6326108641
Mike Wyatt II <mike@mikewyattdesign.com>
2015-08-28 10:47:13 -0500

    try using change column default method

--- db/migrate/20150828140054_default_favorite_comment_booleans_to_zero.rb
+++ db/migrate/20150828140054_default_favorite_comment_booleans_to_zero.rb
@@ -3,14 +3,8 @@ class DefaultFavoriteCommentBooleansToZero < ActiveRecord::Migration
     change_column_null :comments, :event_favorite, false, 0
     change_column_null :comments, :program_favorite, false, 0
     change_column_null :comments, :brand_favorite, false, 0
-    if  Rails.env.production?
-        change_column :comments, :event_favorite, 'boolean USING CAST(event_favorite as boolean)', default: 0
-        change_column :comments, :program_favorite, 'boolean USING CAST(program_favorite as boolean)', default: 0
-        change_column :comments, :brand_favorite, 'boolean USING CAST(brand_favorite as boolean)', default: 0
-    else
-        change_column :comments, :event_favorite, :boolean, default: 0
-        change_column :comments, :program_favorite, :boolean, default: 0
-        change_column :comments, :brand_favorite, :boolean, default: 0
-    end
+    change_column_default :comments, :event_favorite, false
+    change_column_default :comments, :program_favorite, false
+    change_column_default :comments, :brand_favorite, false
   end
 end
