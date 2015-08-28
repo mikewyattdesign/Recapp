212a6a177d85b36ef187413b6c4d2d1255aea330
Mike Wyatt II <mike@mikewyattdesign.com>
2015-08-28 09:54:18 -0500

    update default value change to work with PG and SQLite

--- db/migrate/20150828140054_default_favorite_comment_booleans_to_zero.rb
+++ db/migrate/20150828140054_default_favorite_comment_booleans_to_zero.rb
@@ -3,8 +3,8 @@ class DefaultFavoriteCommentBooleansToZero < ActiveRecord::Migration
     change_column_null :comments, :event_favorite, false, 0
     change_column_null :comments, :program_favorite, false, 0
     change_column_null :comments, :brand_favorite, false, 0
-    change_column :comments, :event_favorite, :boolean, default: 0
-    change_column :comments, :program_favorite, :boolean, default: 0
-    change_column :comments, :brand_favorite, :boolean, default: 0
+    change_column_default :comments, :event_favorite, false
+    change_column_default :comments, :program_favorite, false
+    change_column_default :comments, :brand_favorite, false
   end
 end
