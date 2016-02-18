02b5914a736d26d6ce9c389c4c9359c04b4e27b7
Michael Liu <michaelliu@group360.com>
2016-02-18 16:10:46 -0600

    Incorporate video thumbnail

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,11 @@
 class Video < ActiveRecord::Base
   belongs_to :event
     has_attached_file :media, styles: {
-      original: {geometry: "640x480", format: 'mp4'}
+      original: { format: 'mp4' },
+      medium: { geometry: "640x480#", format: 'mp4' },
+      thumb: { geometry: "300x300#", format: 'jpg',
+        output: { vframes: 1, s: '300x300', ss: '00:00:02' }
+      }
     }, processors: [:transcoder]
     validates_attachment_content_type :media, :content_type => /\Avideo\/.*\z/
     validates_attachment_presence :media
