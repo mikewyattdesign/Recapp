f3b8b600fa109ba0725d576a7e85188f2c17253f
Michael Liu <michaelliu@group360.com>
2016-02-18 17:10:34 -0600

    Try uploading without transcoding

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,11 +1,6 @@
 class Video < ActiveRecord::Base
   belongs_to :event
-    has_attached_file :media, styles: {
-      medium: { format: 'mp4' },
-      thumb: { geometry: "300x300#", format: 'jpg',
-        output: { vframes: 1, s: '300x300', ss: '00:00:02' }
-      }
-    }, processors: [:transcoder]
+    has_attached_file :media
     validates_attachment_content_type :media, :content_type => /\Avideo\/.*\z/
     validates_attachment_presence :media
     validates :event_id, presence: true
